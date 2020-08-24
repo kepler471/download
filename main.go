b0769b220d2cfddb18e48b184719fc160694614b
Stelios Georgiou <steliosdg@gmail.com>
2020-08-24 17:19:13 +0100

    fixed errors in flag control, tabbed output under each URL for clarity

--- main.go
+++ main.go
@@ -17,24 +17,20 @@ var (
 
 func main() {
 	flag.Parse()
-	// TODO want to handle URL endings, eg .html, .htm ...
-	// TODO handle even when 0 urls provided with better feedback
-	// TODO provide help, default for no args
-	// TODO replace logs with errors
 	var (
 		files     []File
 		totalSize uint64
 	)
 	for _, URL := range flag.Args() {
 		if !*l {
-			fmt.Println(URL)
+			fmt.Println("From: ", URL)
 		}
 		links := getLinks(handleURL(URL))
 		for _, link := range links {
 			if strings.HasSuffix(link, "."+*t) {
 				f := getInfo(URL, link)
 				if !*l {
-					fmt.Printf("size: %v, %v,\t%v\n", humanize.Bytes(f.Size), f.Name, f.URL)
+					fmt.Printf("\tsize: %v, %v,\t%v\n", humanize.Bytes(f.Size), f.Name, f.URL)
 				} else {
 					fmt.Println(f.URL)
 				}
@@ -43,29 +39,33 @@ func main() {
 			}
 		}
 	}
-	// UI, suppressed by -l flag
-	if !*l && len(files) > 0 {
-		fmt.Printf("\n%v %v files found\n", len(files), *t)
-		fmt.Printf("Total download size: %v\n", humanize.Bytes(totalSize))
-		if !*y {
-			fmt.Print("Would you like to download all [y/N]: ")
-			var input string
-			_, err := fmt.Scanln(&input)
-			input = strings.Trim(input, " ")
-			if err != nil {
-			}
-			switch input {
-			case "y", "Y":
-				for _, f := range files {
-					f.Download()
-				}
-			default:
-				fmt.Println("Aborted.")
-			}
-		} else {
+	// UI
+	if *l {
+		return
+	}
+	fmt.Printf("\n%v %v files found\n", len(files), *t)
+	if len(files) == 0 {
+		return
+	}
+	fmt.Printf("Total download size: %v\n", humanize.Bytes(totalSize))
+	if !*y {
+		fmt.Print("Would you like to download all [y/N]: ")
+		var input string
+		_, err := fmt.Scanln(&input)
+		input = strings.Trim(input, " ")
+		if err != nil {
+		}
+		switch input {
+		case "y", "Y":
 			for _, f := range files {
 				f.Download()
 			}
+		default:
+			fmt.Println("Aborted.")
+		}
+	} else {
+		for _, f := range files {
+			f.Download()
 		}
 	}
 }
